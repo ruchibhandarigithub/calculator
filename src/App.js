53521d4102001fa6513609614217144b5e5c29e1
unknown <bhandariruchi498@gmail.com>
2023-08-12 13:31:59 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [num1,setNum1] = useState('');
+  const [num2,setNum2] = useState('');
+  const [result,setResult]= useState('');
+  const [error,setError]=useState('');
+  const [success,setSuccess]=useState('');
+  const performOperation =(operator)=>{
+      if(!num1 || !num2  || isNaN(num1) || isNaN(num2)){
+        setError('Error!');
+        if(!num1){
+          setResult('Num1 should not empty');
+        }
+        else{
+          setResult('Num2 should not empty');
+        }
+        
+        setSuccess('')
+        
+
+        
+      }
+      else{
+        setSuccess('Success');
+        setError('');
+      
+      const one = parseFloat(num1);
+      const two = parseFloat(num2);
+      switch(operator){
+        case '+':
+               setResult('Result:  '+(one+two));
+               break;
+        case '-':
+               setResult('Result:  '+(one-two));
+              break;
+        case '*':
+          setResult('Result:  '+(one*two));
+               break; 
+        case '/':
+          setResult('Result:  '+(one/two));
+               break;
+        default:
+                setResult('');   
+      }
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className='calculator'>
+        <h1>React Calculator</h1>
+        <div className='input'>
+        <input type="text" placeholder="Num 1" onChange={(e)=> setNum1(e.target.value)} />
+        <input type="text" placeholder="Num 2" onChange={ (e)=> setNum2(e.target.value)} /> 
+        </div>
+        
+        <div className='button'>
+           <button onClick={()=>performOperation('+')}>+</button>
+           <button onClick={()=>performOperation('-')}>-</button>
+           <button onClick={()=>performOperation('*')}>*</button>
+           <button onClick={()=>performOperation('/')}>/</button>
+        </div>
+        {
+          error && <p className="error">{error}</p>
+        }
+        {
+          success && <p className='success'>{success}</p>
+        }
+        {
+          result && <p className='result'>{result}</p>
+        }
+      </div>
+
+   
   );
 }
 
